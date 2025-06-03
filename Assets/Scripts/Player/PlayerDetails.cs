3c3dfea15c276b6465988dd709821ec43952c43e
TranHuuMinhDuc <ytientvhue@gmail.com>
2025-06-03 18:55:50 +0700

    Sprite & Animation

--- Assets/Scripts/Player/PlayerDetails.cs
+++ Assets/Scripts/Player/PlayerDetails.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerDetails : MonoBehaviour
+namespace Snorx.Data
 {
-    // Start is called before the first frame update
-    void Start()
+    [CreateAssetMenu(fileName = "PlayerDetails", menuName = "ScriptableObjects/Player/PlayerDetails")]
+    public class PlayerDetails : ScriptableObject
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("Player Details")]
+        public string playerName;
+        public float playerSpeed = 5f;
+        public float playerHealth = 100f;
+        public float ImmuneTime = 1f;
+        public bool isImmune;
     }
 }
+
